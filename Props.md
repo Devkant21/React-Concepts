c322c700bb5e07708b02b1c0a88b89a7fa1586cd
Devkant <devkant21@gmail.com>
2022-03-16 14:17:35 +0530

    added state

--- Props.md
+++ Props.md
@@ -22,3 +22,32 @@ const App = () => {
         <Person />
       </div>
     </>
+
+
+<!-- Using Props -->
+
+
+const Avenger = (props) => {
+  return (
+    <>
+      <h1>First Name : {props.name}</h1>
+      <h2>Last Name : {props.lastName}</h2>
+      <h3>Powers : {props.powers}</h3>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <div className='App'>
+      {/* This method is lengthy as one change would result in change in all. The best way to go forward is to use the prop */}
+        <Avenger name={'Iron'} lastName={'Man'} powers={'Iron Man Suit'}/>
+        <Avenger name={'Captain'} lastName={'America'} powers={'Super Soldier'}/>
+        <Avenger name={'Incredible'} lastName={'Hulk'} powers={'Green Soldier'}/>
+      </div>
+    </>
+  )
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -31,29 +31,45 @@ import './App.css';
 // }
 
 
-// Custom Components
+// Custom Components using props
 
-const Avenger = (props) => {
-  return (
-    <>
-      <h1>First Name : {props.name}</h1>
-      <h2>Last Name : {props.lastName}</h2>
-      <h3>Powers : {props.powers}</h3>
-    </>
-  )
-}
+// const Avenger = (props) => {
+//   return (
+//     <>
+//       <h1>First Name : {props.name}</h1>
+//       <h2>Last Name : {props.lastName}</h2>
+//       <h3>Powers : {props.powers}</h3>
+//     </>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className='App'>
+//       {/* This method is lengthy as one change would result in change in all. The best way to go forward is to use the prop */}
+//         <Avenger name={'Iron'} lastName={'Man'} powers={'Iron Man Suit'}/>
+//         <Avenger name={'Captain'} lastName={'America'} powers={'Super Soldier'}/>
+//         <Avenger name={'Incredible'} lastName={'Hulk'} powers={'Green Soldier'}/>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App;
+
+import { useState } from 'react';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <>
-      <div className='App'>
-      {/* This method is lengthy as one change would result in change in all. The best way to go forward is to use the prop */}
-        <Avenger name={'Iron'} lastName={'Man'} powers={'Iron Man Suit'}/>
-        <Avenger name={'Captain'} lastName={'America'} powers={'Super Soldier'}/>
-        <Avenger name={'Incredible'} lastName={'Hulk'} powers={'Green Soldier'}/>
-      </div>
-    </>
+    <div className='App'>
+      <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
+
+    </div>
   )
 }
-
 export default App;
