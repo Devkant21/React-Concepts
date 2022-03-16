7baab22888a4283359b50421b4f665f19e0deda0
Devkant <devkant21@gmail.com>
2022-03-16 12:02:53 +0530

    Create custom components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
-const App = () =>  {
-  const name = 'Dev'
+// const App = () =>  {
+//   const name = 'Dev'
   // const isNameShowing = false
 
 // Dynamic Name or data showing
@@ -13,21 +13,48 @@ const App = () =>  {
 //   );
 // }
 
+//   return (
+//     <div className="App">
+//       <h1>Hello </h1>
+//       {name ? (
+//         <>
+//           <h1>{name}</h1>
+//         </>
+//       ) : (
+//         <>
+//           <h1>test</h1>
+//           <h2>There is no name</h2>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
+
+// Custom Components
+
+const Person = () => {
   return (
-    <div className="App">
-      <h1>Hello </h1>
-      {name ? (
-        <>
-          <h1>{name}</h1>
-        </>
-      ) : (
-        <>
-          <h1>test</h1>
-          <h2>There is no name</h2>
-        </>
-      )}
-    </div>
-  );
+    <>
+      <h1>Name : Jane</h1>
+      <h2>Last Name : Doe</h2>
+      <h3>Age : 32</h3>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <div className='App'>
+      {/* This method is lengthy as one change would result in change in all. The best way to go forward is to use the prop */}
+        <Person />
+        <Person />
+        <Person />
+
+      </div>
+    </>
+  )
 }
 
 export default App;
